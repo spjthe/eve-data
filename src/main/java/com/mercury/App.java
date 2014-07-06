8b430c30da26aea0cad4a8f29606debce1695c8b
Simon Theodosius <simon.theodosius@gmail.com>
2014-07-06 13:39:36 +0100

    Changes the prefix of the datasource properties

--- src/main/java/com/mercury/App.java
+++ src/main/java/com/mercury/App.java
@@ -2,19 +2,36 @@ package com.mercury;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.TomcatDataSourceConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.*;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @Import(RepositoryRestMvcConfiguration.class)
 @ImportResource("classpath:spring-config.xml")
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+}
+
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.ds_eve")
+class DataSourceConfig extends TomcatDataSourceConfiguration {
+  @Bean(name = "dsEveData")
+  public DataSource dataSource() {
+    return super.dataSource();
+  }
+
+  @Bean(name = "jdbcEveData")
+  public JdbcTemplate jdbcTemplate(DataSource dsEveData) {
+    return new JdbcTemplate(dsEveData);
   }
 }
