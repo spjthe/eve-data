d023d6d8972551738e79bc93d589c73c38059d6b
Simon Theodosius <simon.theodosius@gmail.com>
2014-07-13 19:24:08 +0100

    Adds configuration for market entities

--- src/main/java/com/mercury/App.java
+++ src/main/java/com/mercury/App.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@Import(RepositoryRestMvcConfiguration.class)
-@ImportResource("classpath:spring-config.xml")
+@ImportResource("classpath:data-config.xml")
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
